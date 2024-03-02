b59d0a8ef6831725e276ae548b73b63080032836
YANGJUNHO <yangwork96@gmail.com>
2024-03-02 22:17:30 +0900

    alarm click event

--- main.js
+++ main.js
@@ -1,6 +1,37 @@
 import { gsap } from "gsap";
 import tl from "./animation.js/intro.js";
 
+const alarmButton = document.querySelector(".header_alarmButton");
+const alarmSpace = document.querySelector(".alarmSpace");
+
+function alarmColor() {
+  alarmButton.insertAdjacentHTML(
+    "beforeend",
+    '<div class="alarmColor"><span class="ir">알람메세지</span></div>'
+  );
+  return;
+}
+
+function handleAlram(e) {
+  e.preventDefault();
+  if (alarmSpace.classList.contains("virtualClass")) {
+    alarmSpace.classList.remove("virtualClass");
+    gsap.to(".alarmSpace", {
+      x: -345,
+      duration: 1,
+    });
+  } else {
+    alarmSpace.classList.add("virtualClass");
+    gsap.to(".alarmSpace", {
+      x: 0,
+      duration: 1,
+    });
+    // alarmButton.lastChild.remove();
+  }
+}
+
+alarmButton.addEventListener("click", handleAlram);
+
 // 1. 애니메이션이 다 끝나면 알람이미지 흔들리기
 // 2. 알람버튼 누르면 알람 메세지 나오기
 // 3. 다시 눌러서 알람을 확인하면 알람이미지 흔들리지 않기
